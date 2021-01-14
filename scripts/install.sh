8428179fb8a8b0cb6ac7efb078baf839f8cb1942
Faiez Zalila <faiez.zalila@gmail.com>
2021-01-14 15:08:25 +0100

    check environment.

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,12 +3,12 @@
 set -o errexit
 
 HELM_VERSION=3.1.1
-HELM_GCS_VERSION=0.3.1
+HELM_GCS_VERSION=0.3.7
 
-echo "Installing Helm..."
-wget -q https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz
-tar -zxf helm-v${HELM_VERSION}-linux-amd64.tar.gz
-sudo mv linux-amd64/helm /usr/local/bin/helm
+#echo "Installing Helm..."
+#wget -q https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz
+#tar -zxf helm-v${HELM_VERSION}-linux-amd64.tar.gz
+#sudo mv linux-amd64/helm /usr/local/bin/helm
 helm version
 
 echo "Installing helm-gcs plugin..."
